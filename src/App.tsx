241c59149e2cce155c0b2c2a9af410a395f1aa4b
qianyu <280624745@qq.com>
2021-06-12 14:21:39 +0800

    创建 reset.scss helper.scss Nav.tsx 文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,65 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
+import Nav from './components/Nav';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
 
 function App() {
   return (
-    <div>
-      Hi
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav/>
+      </Wrapper>
+    </Router>
   );
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function NoMatch() {
+  return <h2>你访问的页面不存在</h2>;
 }
 
 export default App;
