0728c8e3ef31ce1af880419b4e3200e6e479553e
qianyu <280624745@qq.com>
2021-06-13 18:15:20 +0800

    完成 Notes.tsx 组件

--- src/components/tally/Notes.tsx
+++ src/components/tally/Notes.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {useRef, useState} from 'react';
 
 const NoteSections = styled.section`
   background: #f5f5f5;
@@ -25,11 +26,22 @@ const NoteSections = styled.section`
 `;
 
 function Notes() {
+  const [note, setNote] = useState('');
+  const refInput = useRef<HTMLInputElement>(null);
+  console.log(note);
+  const onBlur = () => {
+    if (refInput.current) {
+      setNote(refInput.current.value);
+    }
+  };
   return (
     <NoteSections>
       <label>
         <span>备注</span>
-        <input type="text" placeholder="在这里添加备注"/>
+        <input type="text" placeholder="在这里添加备注"
+               ref={refInput}
+               defaultValue={note}
+               onBlur={onBlur}/>
       </label>
     </NoteSections>
   );
