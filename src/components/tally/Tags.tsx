4033315f41d179214795bb0ce473db6174498578
qianyu <280624745@qq.com>
2021-06-13 17:27:50 +0800

    完成 Tags.tsx 组件

--- src/components/tally/Tags.tsx
+++ src/components/tally/Tags.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {useState} from 'react';
 
 const TagSections = styled.section`
   flex-grow: 1;
@@ -19,6 +20,11 @@ const TagSections = styled.section`
       padding: 3px 18px;
       font-size: 14px;
       margin: 8px 12px;
+
+      &.selected {
+        background: red;
+        color: white;
+      }
     }
   }
 
@@ -33,15 +39,40 @@ const TagSections = styled.section`
 `;
 
 function Tags() {
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const toggle = (tag: string) => {
+    if (selectedTags.indexOf(tag) >= 0) {
+      setSelectedTags(selectedTags.filter(t => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+  const addTag = () => {
+    const tag = window.prompt('请问新添加的标签名是什么');
+    if (tag !== null) {
+      if (tag) {
+        setTags([...tags, tag]);
+        window.alert('标签名添加成功');
+      } else {
+        window.alert('标签名不能为空');
+      }
+    }
+  };
   return (
     <TagSections>
       <ol>
-        <li>衣</li>
-        <li>食</li>
-        <li>住</li>
-        <li>行</li>
+        {tags.map(tag => {
+          return (
+            <li key={tag}
+                className={selectedTags.indexOf(tag) >= 0 ? 'selected' : ''}
+                onClick={() => toggle(tag)}>
+              {tag}
+            </li>
+          );
+        })}
       </ol>
-      <button>新增标签</button>
+      <button onClick={addTag}>新增标签</button>
     </TagSections>
   );
 }
