b93f0dbc9bc0322fd47b237439e585c7bc423bd8
qianyu <280624745@qq.com>
2021-07-01 12:47:06 +0800

    修改 Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,10 +3,9 @@ import styled from 'styled-components';
 const Button = styled.button`
   font-size: 18px;
   border: none;
-  padding: 8px 12px;
-  background: #f60;
-  border-radius: 4px;
-  color: white;
+  padding: 8px 32px;
+  background: rgba(255, 218, 69, 1);
+  border-radius: 8px;
 `;
 
 export {Button};
