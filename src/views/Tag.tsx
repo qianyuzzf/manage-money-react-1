89635b591e5e8332dff11f5955b67c5424cc3fbe
qianyu <280624745@qq.com>
2021-07-02 15:30:22 +0800

    增加标签字数限制

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -8,7 +8,7 @@ import {Input} from '../components/Input';
 import {Center} from '../components/Center';
 import {Space} from '../components/Space';
 import {model} from '../lib/model';
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const NavBar = styled.nav`
   display: flex;
@@ -34,6 +34,12 @@ const MyInput = styled(Input)`
   height: 44px;
 `;
 
+const Note = styled.div`
+  font-size: 12px;
+  padding: 6px 16px;
+  color: #ed5d5d;
+`;
+
 function Tag() {
   const {findTag, updateTag, deleteTag} = useTags();
   const {id} = useParams<{ id: string }>();
@@ -43,10 +49,23 @@ function Tag() {
     setDefaultName(model().cloneValue(tag.name));
     // eslint-disable-next-line
   }, []);
+  const [newTag, setNewTag] = useState('');
   const history = useHistory();
   const routeBack = () => {
+    const bool = window.confirm('确定保存吗');
+    if (bool) {
+      const reg = /^[\u4e00-\u9fa5]+$/;
+      const reg2 = /^[A-Za-z0-9]+$/;
+      const value = newTag;
+      if (reg.test(value)) {
+        updateTag(tag.id, value.slice(0, 2));
+      } else if (reg2.test(value)) {
+        updateTag(tag.id, value.slice(0, 4));
+      }
+    }
     history.goBack();
   };
+
   return (
     <Layout>
       <NavBar>
@@ -60,13 +79,18 @@ function Tag() {
       </InputWrapper>
       <InputWrapper>
         <MyInput inputName="新标签名" type="text" placeholder="请输入新的标签名"
-                 onChange={(e) => updateTag(tag.id, e.target.value)}/>
+                 onChange={(e) => {
+                   setNewTag(e.target.value);
+                 }}/>
       </InputWrapper>
+      <Note>
+        注：仅保留汉字前两位或数字及字母前四位
+      </Note>
       <Center>
         <Space height={48}/>
         <Button onClick={() => {
           deleteTag(tag.id);
-          window.history.back();
+          history.goBack();
         }}>
           删除标签
         </Button>
